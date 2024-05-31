1920a93f981cadbdb5d6fc9aa65c3129ebf1850d
Shantanu Inagle <shantanuingale2002@gmail.com>
2024-05-31 12:22:37 +0530

    first commit

---
+++ EmployeeTaskCore/Models/LeaveApplyModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTaskCore.Models
+{
+    public class LeaveApplyModel
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public string Reason { get; set; } = string.Empty;
+    }
+}
